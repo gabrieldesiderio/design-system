9dead4830eb5478f960a96f9c5f62bc50d8cfbea
Gabriel Desidério <gabrieldesd7@gmail.com>
2023-05-08 18:54:35 -0300

    add subpath to Storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -17,5 +17,12 @@ const config = {
   docs: {
     autodocs: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/design-system/'
+    }
+
+    return config
+  }
 };
 export default config;
